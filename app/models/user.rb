cd5ef0b7e6a65741ef9081dfc12b77982e2d3b2d
App Academy Student <student@appacademy.io>
2013-12-16 15:12:10 -0800

    create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+   attr_accessible :email, :password
+   attr_accessor :password
+
+   before_validation :set_session_token, :set_password_digest, :on => :create
+   validates :email, :password_digest, :session_token, :presence => true
+
+   def set_session_token
+     self.session_token = SecureRandom::urlsafe_base64(16)
+   end
+
+   def set_password_digest
+     self.password_digest = BCrypt::Password.create(self.password)
+   end
+
+   def reset_session_token!
+     set_session_token
+     self.save!
+   end
+
+   def self.find_by_credentials(email, secret)
+     user = User.find_by_email(email)
+     return user if !user.nil? && user.is_password?(secret)
+     nil
+   end
+
+   def is_password?(secret)
+     BCrypt::Password.new(self.password_digest).is_password?(secret)
+   end
 end
