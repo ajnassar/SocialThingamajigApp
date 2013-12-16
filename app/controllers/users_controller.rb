5793d43ed0064ecfe1659f5917afd88023df34f8
App Academy Student <student@appacademy.io>
2013-12-16 15:20:33 -0800

    create sessionhelper module and SessionsController module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to new_user_url
+    else
+      flash[:errors]=@user.errors.full_messages
+      render :new
+    end
+  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,28 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def log_in_user!(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+
+  def log_out!
+    if logged_in?
+      current_user.reset_session_token!
+      session[:session_token] = nil
+      redirect_to new_session_url
+    end
+  end
+
+  def authenticate
+    redirect_to new_sessions_url unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
